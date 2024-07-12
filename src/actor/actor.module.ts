d7ebd7ec2276b93a4d49d699446ed68459e1e4d2
GezMin <s.shvetsov.v@gmail.com>
2024-07-12 21:45:28 +0500

    update entity actor

--- src/actor/actor.module.ts
+++ src/actor/actor.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
-import { ActorService } from './actor.service'
-import { ActorController } from './actor.controller'
 import { PrismaService } from 'src/prisma.service'
+import { ActorController } from './actor.controller'
+import { ActorService } from './actor.service'
 
 @Module({
 	controllers: [ActorController],
-	providers: [ActorService, PrismaService],
+	providers: [ActorService, PrismaService]
 })
 export class ActorModule {}
